de1740809c54351e4cb1ded3ec30a295e72e0b1b
HDegroote <75906619+HDegroote@users.noreply.github.com>
2023-02-24 10:44:58 +0100

    Add prints in comment

--- test/basic.js
+++ test/basic.js
@@ -10,7 +10,7 @@ test('basic - two writers', async t => {
   const bases = await create(3, apply, store => store.get('test'))
   const [base1, base2, base3] = bases
 
-  printIndexLengths('Init state', bases)
+  printIndexLengths('Init state', bases) // [ 0, 0, 0 ]
 
   // Add base2 as writer
   await base1.append({
@@ -19,12 +19,12 @@ test('basic - two writers', async t => {
   await sync(base1, base2, base3)
   await base1.append(null)
   await sync(base1, base2, base3)
-  printIndexLengths('Synced the add-writer msg:', bases)
+  printIndexLengths('Synced the add-writer msg:', bases) // [ 1, 1, 1 ]
 
   // Each writer adds a message
   await base1.append('1-entry1')
   await base2.append('2-entry1')
-  printIndexLengths('Added new messages, but not synced:', bases)
+  printIndexLengths('Added new messages, but not synced:', bases) // [ 1, 1, 1 ]
 
   // Sync writers, but not reader
   await sync(base1, base2, base3)
@@ -32,15 +32,16 @@ test('basic - two writers', async t => {
   await base1.append(null)
   await base2.append(null)
   await sync(base1, base2)
+  printIndexLengths('First nulls appended and synced:', bases) // [ 1, 1, 1 ]
   // Confirm desire to commit current state
   await base1.append(null)
   await base2.append(null)
   await sync(base1, base2)
-  printIndexLengths('Synced 2 writers, but not reader:', bases)
+  printIndexLengths('Fully synced 2 writers, but not reader:', bases) // [ 3, 3, 1 ]
 
   // Sync reader with one (or both) of the writers
   await sync(base1, base3)
-  printIndexLengths('Reader synced:', bases)
+  printIndexLengths('Reader synced:', bases) // [3, 3  3]
 })
 
 async function apply (batch, view, base) {
