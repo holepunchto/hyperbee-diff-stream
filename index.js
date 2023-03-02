7a3557f7b5942a1ccfa939ccb0bdc02c2e598d6e
HDegroote <75906619+HDegroote@users.noreply.github.com>
2023-03-02 15:23:05 +0100

    Use union-stream's map

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const Union = require('sorted-union-stream')
 const b4a = require('b4a')
-const { Readable, Transform, pipeline } = require('streamx')
 
 function getKey (diffEntry) {
   const { left, right } = diffEntry
@@ -19,49 +18,29 @@ function getDiffs (oldBee, newBee) {
   newBee = newBee.snapshot({ keyEncoding: 'binary', valueEncoding: 'binary' })
 
   const oldIndexedL = oldBee.core.indexedLength
-
-  const origOldDiffStream = oldBee.createDiffStream(oldIndexedL)
-  const oldDiffMapper = new Transform({
-    // left <-> right (~add<->delete) because the oldstream's entries need to be undone
-    transform: (entry, cb) => cb(null, { left: entry.right, right: entry.left, isOld: true })
-  })
-  const oldDiffStream = pipeline(origOldDiffStream, oldDiffMapper)
-
+  const oldDiffStream = oldBee.createDiffStream(oldIndexedL)
   const newDiffStream = newBee.createDiffStream(oldIndexedL)
-  const unionised = new Union(oldDiffStream, newDiffStream, (entry1, entry2) => {
-    const res = b4a.compare(getKey(entry1), getKey(entry2))
-    if (res !== 0) return res
-    if (entry1.isOld === entry2.isOld) return 0
-    return entry1.isOld ? -1 : 1 // Old first
-  })
 
-  const outStream = new Readable()
+  const unionised = new Union(
+    oldDiffStream,
+    newDiffStream,
+    {
+      compare: (e1, e2) => b4a.compare(getKey(e1), getKey(e2)),
+      map: (oldEntry, newEntry) => {
+        if (oldEntry === null) return newEntry
+        // Old entries require undoing, so reverse
+        if (newEntry === null) return { seq: oldEntry.seq, left: oldEntry.right, right: oldEntry.left }
 
-  let bufferedEntry = null
-  const processEntry = (entry) => {
-    if (bufferedEntry && b4a.equals(getKey(bufferedEntry), getKey(entry))) {
-      const oldEntry = bufferedEntry
-      const newEntry = entry
-
-      const leftEq = areEqual(oldEntry.right, newEntry.left)
-      const rightEq = areEqual(oldEntry.left, newEntry.right)
-      if (!(leftEq && rightEq)) { // else: already processed in prev getDiffs
-        outStream.push(entry)
+        const leftEq = areEqual(oldEntry.left, newEntry.left)
+        const rightEq = areEqual(oldEntry.right, newEntry.right)
+        if (!(leftEq && rightEq)) return rightEq
+        // else: already processed in prev getDiffs, so filter out
+        return null
       }
-      bufferedEntry = null
-    } else {
-      if (bufferedEntry) outStream.push(bufferedEntry)
-      bufferedEntry = entry
     }
-  }
+  )
 
-  unionised.on('data', processEntry)
-  unionised.on('close', () => {
-    if (bufferedEntry) outStream.push(bufferedEntry)
-    outStream.push(null)
-  })
-
-  return outStream
+  return unionised
 }
 
 module.exports = getDiffs
