626d21d6061d893dcedecd2204c82a95a7440414
HDegroote <75906619+HDegroote@users.noreply.github.com>
2023-03-02 10:20:37 +0100

    Merge streams

--- index.js
+++ index.js
@@ -1,48 +1,59 @@
 const sameObject = require('same-object')
+const Union = require('sorted-union-stream')
+const b4a = require('b4a')
+const { Readable, Transform, pipeline } = require('streamx')
 
-function getChangedKey (diffEntry) {
+function getKey (diffEntry) {
   const { left, right } = diffEntry
   return left ? left.key : right.key
 }
 
-async function shouldAddNewEntry (newEntry, oldBee) {
-  const key = getChangedKey(newEntry)
-  const oldEntry = await oldBee.get(key)
-  // The seqNr does not matter: can change on truncates while value remains same
-  // (so we only compare values)
-  // Note: all deletion info is also contained in .left, so no need to
-  // have a look at .right--we only care about the current state of the newEntry
-  // (not where it changed from)
-  const newChanged = !sameObject(oldEntry?.value, newEntry.left?.value)
-
-  return newChanged
-}
-
 async function getDiffs (oldBee, newBee) {
   const oldIndexedL = oldBee.core.indexedLength
-  const oldIndexedBee = oldBee.checkout(oldIndexedL) // Same as newBee.checkout(oldIndexedL)
 
-  const res = []
+  const origOldDiffStream = oldBee.createDiffStream(oldIndexedL)
+  const oldDiffMapper = new Transform({
+    // left <-> right (~add<->delete) because the oldstream's entries need to be undone
+    transform: (entry, cb) => cb(null, { left: entry.right, right: entry.left, isOld: true })
+  })
+  const oldDiffStream = pipeline(origOldDiffStream, oldDiffMapper)
 
-  for await (const newEntry of newBee.createDiffStream(oldIndexedL)) {
-    if (await shouldAddNewEntry(newEntry, oldBee)) {
-      res.push(newEntry)
+  const newDiffStream = newBee.createDiffStream(oldIndexedL)
+  const unionised = new Union(oldDiffStream, newDiffStream, (entry1, entry2) => {
+    const res = b4a.compare(getKey(entry1), getKey(entry2))
+    if (res !== 0) return res
+    if (entry1.isOld === entry2.isOld) return 0
+    return entry1.isOld ? -1 : 1 // Old first
+  })
+
+  const outStream = new Readable()
+
+  let bufferedEntry = null
+  const processEntry = (entry) => {
+    if (bufferedEntry && b4a.equals(getKey(bufferedEntry), getKey(entry))) {
+      const oldEntry = bufferedEntry
+      const newEntry = entry
+
+      // TODO: binary checkouts for b4a equals
+      const leftEq = sameObject(oldEntry.right?.value, newEntry.left?.value)
+      const rightEq = sameObject(oldEntry.left?.value, newEntry.right?.value)
+      if (!(leftEq && rightEq)) { // else: already processed in prev getDiffs
+        outStream.push(entry)
+      }
+      bufferedEntry = null
+    } else {
+      if (bufferedEntry) outStream.push(bufferedEntry)
+      bufferedEntry = entry
     }
   }
 
-  for await (const entry of oldBee.createDiffStream(oldIndexedL)) {
-    const key = getChangedKey(entry)
+  unionised.on('data', processEntry)
+  unionised.on('close', () => {
+    if (bufferedEntry) outStream.push(bufferedEntry)
+    outStream.push(null)
+  })
 
-    const valueOld = (await oldIndexedBee.get(key))
-    const valueNew = (await newBee.get(key))
-    const shouldAdd = sameObject(valueOld, valueNew) && !sameObject(entry.left, valueNew) // no change
-    if (shouldAdd) {
-      // Undo, so add<->delete (left<->right)
-      res.push({ seq: entry.seq, left: entry.right, right: entry.left })
-    }
-  }
-
-  return res
+  return outStream
 }
 
 module.exports = getDiffs
