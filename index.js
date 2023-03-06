f8f1288d75893aecae4a8470bb9cf084793249a7
HDegroote <75906619+HDegroote@users.noreply.github.com>
2023-03-06 20:52:35 +0100

    let bee handle keyEncoding

--- index.js
+++ index.js
@@ -14,68 +14,49 @@ function areEqual (diff1, diff2) {
 }
 
 function unionCompare (e1, e2) {
-  return b4a.compare(getKey(e1), getKey(e2))
-}
+  const k1 = getKey(e1)
+  const k2 = getKey(e2)
 
-function conditionalDecoder (encoding) {
-  return encoding
-    ? entry => encoding.decode(entry)
-    : entry => entry
-}
-
-function decoderFactory ({ keyEncoding, valueEncoding }) {
-  const decodeKey = conditionalDecoder(keyEncoding)
-  const decodeValue = conditionalDecoder(valueEncoding)
-
-  return function decode (diffEntry) {
-    const res = { left: null, right: null }
-    if (diffEntry.left) {
-      res.left = {
-        seq: diffEntry.left.seq,
-        key: decodeKey(diffEntry.left.key),
-        value: decodeValue(diffEntry.left.value)
-      }
-    }
-    if (diffEntry.right) {
-      res.right = {
-        seq: diffEntry.right.seq,
-        key: decodeKey(diffEntry.right.key),
-        value: decodeValue(diffEntry.right.value)
-      }
-    }
-    return res
+  if (b4a.isBuffer(k1)) return b4a.compare(k1, k2)
+  else if (typeof k1 === 'string') {
+    if (k1 > k2) return 1
+    return k1 < k2 ? -1 : 0
   }
+
+  throw new Error('Only string or buffer supported')
 }
 
-function unionMapFactory (decoderOpts) {
-  const decode = decoderFactory(decoderOpts)
+function decodeValue (diffEntry, valueEncoding) {
+  if (!valueEncoding) return diffEntry
+
+  if (diffEntry.left) diffEntry.left.value = valueEncoding.decode(diffEntry.left.value)
+  if (diffEntry.right) diffEntry.right.value = valueEncoding.decode(diffEntry.right.value)
+
+  return diffEntry
+}
+
+function unionMapFactory (valueEncoding) {
+  const decode = diffEntry => decodeValue(diffEntry, valueEncoding)
 
   return function unionMap (oldEntry, newEntry) {
     if (oldEntry === null) return decode(newEntry)
-    // Old entries require undoing, so reverse
     if (newEntry === null) {
-      return decode({ seq: oldEntry.seq, left: oldEntry.right, right: oldEntry.left })
+      // Old entries require undoing, so reverse
+      return decode({ left: oldEntry.right, right: oldEntry.left })
     }
 
     const leftEq = areEqual(oldEntry.left, newEntry.left)
     const rightEq = areEqual(oldEntry.right, newEntry.right)
-    if (!(leftEq && rightEq)) return decode(rightEq)
+    if (!(leftEq && rightEq)) return decode(newEntry) // newest wins
     // else: already processed in prev getDiffs, so filter out
     return null
   }
 }
 
-function enc (e, v) {
-  if (v === undefined || v === null) return null
-  if (e !== null) return e.encode(v)
-  if (typeof v === 'string') return b4a.from(v)
-  return v
-}
-
 class BeeDiffStream extends Union {
   constructor (oldBee, newBee, opts = {}) {
-    const keyEncoding = opts.keyEncoding ? codecs(opts.keyEncoding) : oldBee.keyEncoding
     const valueEncoding = opts.valueEncoding ? codecs(opts.valueEncoding) : oldBee.valueEncoding
+    opts = { ...opts, valueEncoding: undefined }
 
     // A normal bee doesn't have indexedLength.
     // In this case, we fallback to the version,
@@ -83,23 +64,15 @@ class BeeDiffStream extends Union {
     const oldIndexedL = oldBee.core.isAutobase ? oldBee.core.indexedLength : oldBee.version
 
     // Binary encodings for easier comparison later
-    oldBee = oldBee.snapshot({ keyEncoding: 'binary', valueEncoding: 'binary' })
-    newBee = newBee.snapshot({ keyEncoding: 'binary', valueEncoding: 'binary' })
+    oldBee = oldBee.snapshot({ keyEncoding: opts.keyEncoding, valueEncoding: 'binary' })
+    newBee = newBee.snapshot({ keyEncoding: opts.keyEncoding, valueEncoding: 'binary' })
 
-    const range = {}
-    if (keyEncoding) {
-      if (opts.gt !== undefined) range.gt = enc(keyEncoding, opts.gt)
-      if (opts.gte !== undefined) range.gte = enc(keyEncoding, opts.gte)
-      if (opts.lt !== undefined) range.lt = enc(keyEncoding, opts.lt)
-      if (opts.lte !== undefined) range.lte = enc(keyEncoding, opts.lte)
-    }
-
-    const oldDiffStream = oldBee.createDiffStream(oldIndexedL, range)
-    const newDiffStream = newBee.createDiffStream(oldIndexedL, range)
+    const oldDiffStream = oldBee.createDiffStream(oldIndexedL, opts)
+    const newDiffStream = newBee.createDiffStream(oldIndexedL, opts)
 
     super(oldDiffStream, newDiffStream, {
       compare: unionCompare,
-      map: unionMapFactory({ keyEncoding, valueEncoding }),
+      map: unionMapFactory(valueEncoding),
       ...opts
     })
   }
