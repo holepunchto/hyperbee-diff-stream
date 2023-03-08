979d53319aebb15d7f01757ae6c849e05484fd00
HDegroote <75906619+HDegroote@users.noreply.github.com>
2023-03-08 19:26:48 +0100

    Complete readme and example (#11)
    * Flesh out readme
    
    * Clean up example
    
    * Add test to ensure example works
    
    * Rm .solo
    
    * Format
    
    * Format

--- example.js
+++ example.js
@@ -5,6 +5,7 @@ const Autobase = require('autobase-next')
 const Corestore = require('corestore')
 
 async function main () {
+  // Setting up a simple hyperbee view for an autobase
   const opts = {
     apply: async (batch, view) => {
       try {
@@ -16,30 +17,49 @@ async function main () {
         console.error(e)
       }
     },
-    open: linStore => new Hyperbee(linStore.get('abee'), { extension: false }),
-    valueEncoding: 'json'
+    open: linStore => new Hyperbee(linStore.get('abee'), {
+      extension: false,
+      keyEncoding: 'utf-8',
+      valueEncoding: 'utf-8'
+    }),
+    valueEncoding: 'json' // the apply function will receive batches of jsons
   }
 
   const store = new Corestore(ram)
   const base = new Autobase(store, null, opts)
   await base.append({ add: ['e1', 'entry1'] })
   await base.append({ add: ['e2', 'entry2'] })
+
   const oldSnap = base.view.snapshot()
 
+  await base.append({ add: ['e3', 'A'] })
+  await base.append({ add: ['e3', 'lot'] })
+  await base.append({ add: ['e3', 'of'] })
+  await base.append({ add: ['e3', 'changes'] })
+  await base.append({ add: ['e3', 'to'] })
   await base.append({ add: ['e3', 'entry3'] })
+
   await base.append({ del: 'e1' })
+
+  await base.append({ add: ['e2', 'new entry 2'] })
   await base.append({ del: 'e2' })
-  await base.append({ add: ['e1', 'new entry1'] })
+
+  await base.append({ add: ['e1', 'Something-else'] })
 
   const newSnap = base.view.snapshot()
-  console.log('old version:', oldSnap.version, '--new version:', newSnap.version)
 
   const diffStream = new BeeDiffStream(oldSnap, newSnap)
   for await (const { left: added, right: removed } of diffStream) {
-    if (added && !removed) console.log('Added:', added.key.toString())
-    if (!added && removed) console.log('Removed: ', removed.key.toString())
-    if (added && removed) console.log('Updated', added.key.toString(), 'from ', removed.value.toString(), ' to', added.value.toString())
+    if (added && !removed) console.log('- Set', added.key, 'to', added.value)
+    if (!added && removed) console.log('- Removed', removed.key)
+    if (added && removed) console.log('- Updated', added.key, 'from', removed.value, 'to', added.value)
   }
+
+  /*
+    - Updated e1 from entry1 to Something-else
+    - Removed e2
+    - Added e3 entry3
+  */
 }
 
 main()
